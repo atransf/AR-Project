deac64b7fd453ee9ec8907f8e1a87874f657a1cb
Anh Tran <lamanh309@gmail.com>
2021-10-15 16:33:40 -0700

    final fix bug

--- AR Project/ContentView.swift
+++ AR Project/ContentView.swift
@@ -88,18 +88,54 @@ struct ARViewContainer: UIViewRepresentable {
 }
 
 class CustomARView: ARView{
-    let focusSquare = FocusEntity()
-    
+    enum FocusStyleChoices {
+      case classic
+      case material
+      case color
+    }
+
+    /// Style to be displayed in the example
+    let focusStyle: FocusStyleChoices = .classic
+    var focusEntity: FocusEntity?
     required init(frame frameRect: CGRect) {
-        super.init(frame: frameRect)
-        
-        focusSquare.delegate = self
-        focusSquare.setAutoUpdate(to: true)
+      super.init(frame: frameRect)
+      self.setupConfig()
+
+      switch self.focusStyle {
+      case .color:
+        self.focusEntity = FocusEntity(on: self, focus: .plane)
+      case .material:
+        do {
+          let onColor: MaterialColorParameter = try .texture(.load(named: "Add"))
+          let offColor: MaterialColorParameter = try .texture(.load(named: "Open"))
+          self.focusEntity = FocusEntity(
+            on: self,
+            style: .colored(
+              onColor: onColor, offColor: offColor,
+              nonTrackingColor: offColor
+            )
+          )
+        } catch {
+          self.focusEntity = FocusEntity(on: self, focus: .classic)
+          print("Unable to load plane textures")
+          print(error.localizedDescription)
+        }
+      default:
+        self.focusEntity = FocusEntity(on: self, focus: .classic)
+      }
         self.setupARView()
     }
-    @objc required dynamic init?(coder decoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+
+    func setupConfig() {
+      let config = ARWorldTrackingConfiguration()
+      config.planeDetection = [.horizontal, .vertical]
+      session.run(config, options: [])
     }
+
+    @objc required dynamic init?(coder decoder: NSCoder) {
+      fatalError("init(coder:) has not been implemented")
+    }
+
     func setupARView() {
         let config = ARWorldTrackingConfiguration()
         config.planeDetection = [.horizontal, .vertical]
